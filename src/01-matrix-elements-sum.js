612435115d725a38fc0766e7035d3d7c6934a5e5
HannaHurniak <hanna.hurniak@yahoo.com>
2021-04-13 02:03:46 +0300

    feat: add some solution

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,18 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  return matrix.reduce((acc, el, index, arr) => {
+    if (index === 0) {
+      return el.reduce((a, elem) => a + elem, 0);
+    }
+    return acc + el.reduce((a, elem, ind) => {
+      if (arr[index - 1][ind] !== 0) {
+        return a + elem;
+      }
+      return a;
+    }, 0);
+  }, 0);
 }
 
 module.exports = getMatrixElementsSum;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,31 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  // const arr = [];
+  // const array = [];
+  // const number = String(n).split('');
+  // const result = String(n).split('');
+  // const resultnew = result.sort((a, b) => a - b);
+  // arr.push(resultnew[0]);
+  // for (let i = 0; i < number.length; i++) {
+  //   if (number[i] !== arr[0]) {
+  //     array.push(number[i]);
+  //   }
+  //   return array;
+  // }
+  // const d = array.join('');
+  // return Number(d);
+  const arr = String(n).split('');
+  const numbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    const result = arr[i];
+    arr.splice(i, 1);
+    const number = Number(arr.join(''));
+    numbers.push(number);
+    arr.splice(i, 0, result);
+  }
+  return Math.max(...numbers);
 }
 
 module.exports = deleteDigit;
